0a51e65b8f628eba71ed135430187a1de7d5df3b
Jakub Beránek <berykubik@gmail.com>
2014-12-11 22:32:50 +0100

    Přidána animace.

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="utf-8">
     <title>MHD</title>
-    <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="style.css" />
 
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
@@ -20,15 +20,14 @@
     <div>
         <h1>Vyhledání spoje</h1>
     </div>
-
-    <form method="POST" action="spoje.php">
+    <div class="formular">
         <div><label for="odkud" id="odkud_label">Odkud</label></div>
         <div><input id="odkud" type="text" value="Rektorát VŠB" /></div>
         <div><label for="kam" id="kam_label">Kam</label></div>
         <div><input id="kam" type="text" value="Svinov mosty, h. z." /></div>
 
-        <button class="btn btn-success">Najít spoj</button>
-    </form>
+        <button class="btn btn-success najit-spoj">Najít spoj</button>
+    </div>
 </div>
 <div class="screen" id="screen2">
     <div>
@@ -46,3 +45,7 @@
         </div>
     </div>
 </div>
+
+</div>
+</body>
+</html>
--- script.ts
+++ script.ts
@@ -1,11 +1,33 @@
 /// <reference path="jquery.d.ts" />
 
-var $ACTIVE_SCREEN = 'screen_1';
+var $ACTIVE_SCREEN : JQuery;
 
 $(() => {
-    $("#screen1").show();
+    $(".najit-spoj").on('click', () => {
+        switchToScreen($("#screen2"));
+    });
+
+    $ACTIVE_SCREEN = $("#screen1");
+    $ACTIVE_SCREEN.show();
 });
 
-function switchToScreen(screen : string) {
+function switchToScreen(screen : JQuery) {
+    screen.css('position', 'absolute');
+    screen.css('left', '1000px');
+    screen.css('top', 0);
+    screen.show();
+    screen.css('opacity', 0);
 
+    var offset : number = $ACTIVE_SCREEN.offset().left;
+
+    screen.animate({
+        left: offset,
+        opacity: 100
+    }, 1000, () => {
+        screen.css('position', 'relative');
+        screen.css('left', 0);
+    });
+    $ACTIVE_SCREEN.fadeOut(1000);
+
+    $ACTIVE_SCREEN = screen;
 }
